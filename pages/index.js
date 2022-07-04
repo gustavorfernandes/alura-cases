b793c8dc7604a80daa6ddfdc6e31e574901a30c3
Gustavo <gustavo.ribeiro.fernandes1@gmail.com>
2022-07-04 15:10:56 -0300

    Building the project

---
+++ pages/index.js
@@ -0,0 +1,37 @@
+import HomeScreen from "../src/screens/HomeScreen";
+
+export default HomeScreen;
+
+// import Footer from "../src/components/Footer";
+// import Link from "../src/components/Link";
+
+// function Title({ children, as }) {
+//   const Tag = as;
+//   return (
+//     <>
+//     <Tag>
+//       {children}
+//     </Tag>
+//     <style jsx>{`
+//       ${Tag} {
+//         color: red;
+//         font-family: sans-serif;
+//       }    
+//     `}</style>
+//     </>
+//   )
+// }
+
+// export default function HomePage() {
+//   return (
+//     <div>
+//       <Title as="h2">
+//         Alura Cases - Home
+//       </Title>
+//       <Link href="/faq">
+//         Ir para a página de FAQ
+//       </Link>
+//       <Footer />
+//     </div>
+//   )
+// }
